b324d4c306d3a2f1f8a767f1e13487e2989afa93
Fukki <takungwong@gmail.com>
2018-09-26 21:03:42 +0700

    :x

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 module.exports = function AutoLFGLink(mod) {
-    const cmd = mod.command || mod.require.command; let interval, spamLFG = 1;
-    function sendLink () {for(let i = 1; i <= spamLFG; i++) mod.send('C_REQUEST_PARTY_MATCH_LINK', 1, {});}
+	const cmd = mod.command || mod.require.command; let interval, spamLFG = 1;
+	function sendLink () {for(let i = 1; i <= spamLFG; i++) mod.send('C_REQUEST_PARTY_MATCH_LINK', 1, {});}
 	mod.hook('S_RETURN_TO_LOBBY', 'raw', () => {clearInterval(interval);});
-    cmd.add('autolfg', (delay, spam) => {
-        delay = Number(delay); spamLFG = Number(spam);
+	cmd.add('autolfg', (delay, spam) => {
+		delay = Number(delay); spamLFG = Number(spam);
 		if (delay > 0) {
 			if (spamLFG < 1) spamLFG = 1;
-            interval = setInterval(sendLink, delay);
-            cmd.message('Auto LFG Link set to: ' + (spamLFG > 0 ? spamLFG + '/' : '1/') + delay + ' ms.');
-        } else {
-            clearInterval(interval);
-            cmd.message('Auto LFG Link has stopped.');
-        }
-    });
+			interval = setInterval(sendLink, delay);
+			cmd.message('Auto LFG Link set to: ' + (spamLFG > 0 ? spamLFG + '/' : '1/') + delay + ' ms.');
+		} else {
+			clearInterval(interval);
+			cmd.message('Auto LFG Link has stopped.');
+		}
+	});
 }
