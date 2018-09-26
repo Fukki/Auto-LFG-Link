464802119ef0f3347001308be1b8cc6c985bded2
Fukki <takungwong@gmail.com>
2018-09-27 00:15:25 +0700

    :/

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 module.exports = function AutoLFGLink(mod) {
 	const cmd = mod.command || mod.require.command; let interval, spamLFG = 1;
-	function sendLink () {for(let i = 1; i <= spamLFG; i++) mod.send('C_REQUEST_PARTY_MATCH_LINK', 1, {});}
 	mod.hook('S_RETURN_TO_LOBBY', 'raw', () => {clearInterval(interval);});
+	function sendLink () {if (spamLFG > 1) {for(let i = 1; i <= spamLFG; i++) _send();} else {_send();}}
+	function _send() {mod.send('C_REQUEST_PARTY_MATCH_LINK', 1, {});}
 	cmd.add('autolfg', (delay, spam) => {
 		delay = Number(delay); spamLFG = Number(spam);
 		if (delay > 0) {
